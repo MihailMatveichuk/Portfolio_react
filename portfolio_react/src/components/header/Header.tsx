a69f44b38baf08ae610e96c6ab7a1c40f73cd68b
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-06-04 20:35:52 +0300

    feat: add header styles

--- portfolio_react/src/components/header/Header.tsx
+++ portfolio_react/src/components/header/Header.tsx
@@ -1,9 +1,26 @@
-import "./Header.scss"
+import CTA from "./CTA";
+import Photo from "../../assets/People.jpg";
+import "./Header.scss";
+import Social from "./Social";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Mikhail Matveichuk</h1>
+        <h5 className="text-light">JavaScript Developer</h5>
+        <CTA />
+        <Social />
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+        <div className="my-photo">
+          <img src={Photo} alt="Photo" />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
