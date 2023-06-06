0f8ac55376df7b5c97720c6a5324be88d014d995
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-06-06 15:51:13 +0300

    feat: add styles

--- portfolio_react/src/components/experience/Article.tsx
+++ portfolio_react/src/components/experience/Article.tsx
@@ -2,12 +2,13 @@ import { BsPatchCheckFill } from "react-icons/bs";
 import { IArticle } from "../../types";
 
 const Article = (data: IArticle) => {
-  console.log(data.title);
   return (
     <article className="experience__details">
-      <BsPatchCheckFill />
-      <h4>{data.title}</h4>
-      <small className="text-light">{data.description}</small>
+      <BsPatchCheckFill className="experience__details-icon" />
+      <div>
+        <h4>{data.title}</h4>
+        <small className="text-light">{data.description}</small>
+      </div>
     </article>
   );
 };
